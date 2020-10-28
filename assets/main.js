feee4d89f9580c0812a2c726f2f8b6f3f77c2081
FrancescoDale <fd060862@gmail.com>
2020-10-28 22:54:45 +0100

    usando mouse enter, dopo il click, al passaggio del mouse appaiono i menu ( che poi andranno separati )

--- assets/main.js
+++ assets/main.js
@@ -3,6 +3,12 @@ $(document).ready(function() {
     // prendo il click su una delle a nel right_menu
     $('.right_menu a').click(function() {
         console.log('pronto!');
+
+        // mostro il Menu
+        $('.right_menu a').mouseenter(function() {
+            $('.drop').addClass('drop_active');
+        }
+        );
     }
     );
 
