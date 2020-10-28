95ae53efc0f7f124319524e295b7212e792302e2
FrancescoDale <fd060862@gmail.com>
2020-10-28 22:43:55 +0100

    click accettato

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
 
     // prendo il click su una delle a nel right_menu
+    $('.right_menu a').click(function() {
+        console.log('pronto!');
+    }
+    );
 
 }
 );
